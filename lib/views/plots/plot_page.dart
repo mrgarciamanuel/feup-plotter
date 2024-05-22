7db6943245d4ed6b1ae80dd17cfe417723917374
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-05-23 00:32:22 +0100

    comments

---
+++ lib/views/plots/plot_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PlotPage extends StatefulWidget {
+  const PlotPage({super.key});
+
+  @override
+  State<PlotPage> createState() => _PlotPageState();
+}
+
+class _PlotPageState extends State<PlotPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Plot Page'),
+    );
+  }
+}
