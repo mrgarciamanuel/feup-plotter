1c6e8d2224e13157b52d150ad770105c37ca2d1c
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-05-23 00:28:01 +0100

    alterações

--- lib/feup_plotter.dart
+++ lib/feup_plotter.dart
@@ -2,17 +2,34 @@ library feup_plotter;
 
 import 'package:flutter/material.dart';
 
-class CustomButtom extends StatelessWidget {
-  var onPressed;
-  final String text;
+/// import 'package:flutter/material.dart';A Calculator.
+///
+/*
+class Calculator {
+  /// import 'package:flutter/material.dart';Returns [value
+  /// ] plus 1.
+  int addOne(int value) => value + 1;
+}*/
 
-  CustomButtom({required this.text, this.onPressed});
+class CustomButton extends StatelessWidget {
+  var onPressed;
+  final Widget child;
+  var style;
+  const CustomButton(
+      {/*super.key*/ Key key, this.onPressed, this.child, this.style})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: onPressed,
-      child: Text(text),
+      style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(16.0),
+          primary: Colors.white,
+          backgroundColor: Colors.white,
+          elevation: 9.0,
+          textStyle: const TextStyle(fontSize: 20)),
+      child: child,
     );
   }
 }
