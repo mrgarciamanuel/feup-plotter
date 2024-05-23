20cf675fb86ebbb1402bef0b8f510c0ef45827c7
Garcia Manuel <garcia.manuel@sincelo.pt>
2024-05-23 17:14:09 +0100

    types of charts constant

---
+++ lib/controllers/constant_and_values.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+var charts = [
+  const DropdownMenuItem(
+    value: "line",
+    child: Text("Line Graph"),
+  ),
+  const DropdownMenuItem(
+    value: "area",
+    child: Text("Area Graph"),
+  ),
+];
